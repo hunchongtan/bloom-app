8bb09d1fcb82a9d2ee0e724dd60dde37207b1d68
Hun Chong Tan <tanhunchong01@gmail.com>
2024-08-01 02:08:16 +0800

    newentryalt

--- frontend/src/components/home/newentry/NewEntryAlt.js
+++ frontend/src/components/home/newentry/NewEntryAlt.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import styles from '../../../styles/home/newentry/NewEntryAlt.module.css';
 import JournalsNav from "./JournalsNav";
 import { MdArrowForwardIos, MdArrowBackIos } from "react-icons/md";
 import DateTimeHeader from './DateTimeHeader';
 import botMessages from '../../../botMessages.json';
 
-const NewEntry = () => {
+const NewEntryAlt = () => {
   const [input, setInput] = useState('');
   const [showPopup, setShowPopup] = useState(false);
   const [canSendMessage, setCanSendMessage] = useState(false);
@@ -17,10 +17,10 @@ const NewEntry = () => {
   const userId = localStorage.getItem('userId');
   const today = new Date().toISOString().split('T')[0];
 
-  const getPromptMessages = () => {
+  const getPromptMessages = useCallback(() => {
     const prompts = botMessages.find(message => message.date === today)?.prompts || [];
     return prompts.map(prompt => prompt.text) || ['No prompt available for today.'];
-  };
+  }, [today]);
 
   useEffect(() => {
     const initialize = async () => {
@@ -60,7 +60,7 @@ const NewEntry = () => {
     };
 
     initialize();
-  }, [userId, today]);
+  }, [userId, today, getPromptMessages]);
 
   const fetchChats = async (userId) => {
     try {
@@ -318,4 +318,4 @@ const NewEntry = () => {
   );
 };
 
-export default NewEntry;
+export default NewEntryAlt;
